646a0cef35241087bfe285a0a7fdff44fe3ade59
Todd Cermak <tcermak@bloomhealthco.com>
2018-10-18 13:55:34 -0500

    changes

--- engine.py
+++ engine.py
@@ -15,11 +15,9 @@ def main():
     entities = [npc, player]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
-
     libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
 
     con = libtcod.console_new(screen_width, screen_height)
-
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
